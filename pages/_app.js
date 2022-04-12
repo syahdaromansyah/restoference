6a7fab4444f1e7207a2b96facb6f6dc356e7ceaa
Syahda Romansyah <syahdarom@gmail.com>
2022-04-12 14:41:25 +0700

    Wrap all pages with LayoutApp component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css';
+import LayoutApp from '../components/LayoutApp';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutApp>
+      <Component {...pageProps} />
+    </LayoutApp>
+  );
 }
