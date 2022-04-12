ceb8374b3d75500442a39d6450cc942fbc83715e
Syahda Romansyah <syahdarom@gmail.com>
2022-04-12 23:24:21 +0700

    Add head content on Explore page

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -1,3 +1,11 @@
+import Head from 'next/head';
+
 export default function Explore() {
-  return <div className='ExplorePage'></div>;
+  return (
+    <div className='ExplorePage'>
+      <Head>
+        <title>Explore Restaurant | RestoFerence</title>
+      </Head>
+    </div>
+  );
 }
