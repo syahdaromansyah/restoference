e63c82cde319799d9d59441056fb8de30950a99a
Syahda Romansyah <syahdarom@gmail.com>
2022-04-12 21:46:27 +0700

    Add images domains at next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['restaurant-api.dicoding.dev'],
+  },
 };
 
 module.exports = nextConfig;
