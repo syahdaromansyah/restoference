87edac2bc49f62a118bede38cc78ef68fed9ce70
Syahda Romansyah <syahdarom@gmail.com>
2022-04-12 14:05:14 +0700

    Add basic reset style page

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang='en' className='scroll-smooth'>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
@@ -15,7 +15,7 @@ export default function Document() {
           rel='stylesheet'
         />
       </Head>
-      <body>
+      <body className='bg-slate-100 font-inter text-slate-800'>
         <Main />
         <NextScript />
       </body>
