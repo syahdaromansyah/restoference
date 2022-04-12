516815ee727f42cc913c39f0456c5b008afbaf45
Syahda Romansyah <syahdarom@gmail.com>
2022-04-12 17:41:08 +0700

    Add head content on Home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return <div className='HomePage'></div>;
+  return (
+    <div className='HomePage'>
+      <Head>
+        <title>Find Restaurant With No Time | RestoFerence</title>
+      </Head>
+    </div>
+  );
 }
