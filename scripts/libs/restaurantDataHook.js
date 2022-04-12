de47432d095d7dc9f566d932016f64e11577b436
Syahda Romansyah <syahdarom@gmail.com>
2022-04-12 22:03:49 +0700

    Add useRestaurantList hook for fetch restaurant lists data

---
+++ scripts/libs/restaurantDataHook.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import useSWR from 'swr';
+import API_ENDPOINT from '../globals/API_ENDPOINT';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const useRestaurantLists = (initialTotalLists = 6) => {
+  const { data, error } = useSWR(API_ENDPOINT.LISTS, fetcher);
+  const [totalPartialLists, setTotalPartialLists] = useState(initialTotalLists);
+
+  return {
+    restaurantLists: data?.restaurants,
+    totalRestaurantLists: data?.count,
+    restaurantPartialLists: data?.restaurants.slice(0, totalPartialLists),
+    totalRestaurantPartialLists: totalPartialLists,
+    addRestaurantPartialLists: (addNumber) => {
+      const totalAddedPartial = addNumber + totalPartialLists;
+      if (totalAddedPartial >= data?.count)
+        return setTotalPartialLists(data?.count);
+      if (totalAddedPartial === data?.count) return;
+      return setTotalPartialLists((prevValue) => prevValue + addNumber);
+    },
+    isLoading: !data && !error,
+    isError: error,
+  };
+};
+
+export { useRestaurantLists };
