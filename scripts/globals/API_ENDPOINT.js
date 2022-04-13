d92a0299fac7be34b9dfb4d08e67fe49d07e1eee
Syahda Romansyah <syahdarom@gmail.com>
2022-04-13 22:43:13 +0700

    Add detail and review API endpoint

--- scripts/globals/API_ENDPOINT.js
+++ scripts/globals/API_ENDPOINT.js
@@ -2,6 +2,8 @@ import CONFIG from './CONFIG';
 
 const API_ENDPOINT = {
   LISTS: `${CONFIG.BASE_API}list`,
+  DETAIL: (restaurantId) => `${CONFIG.BASE_API}detail/${restaurantId}`,
+  REVIEW: `${CONFIG.BASE_API}review`,
 };
 
 export default API_ENDPOINT;
