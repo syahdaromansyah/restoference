59e0e4f269cbc3347031d64792111a82d716e4a6
Syahda Romansyah <syahdarom@gmail.com>
2022-04-14 23:16:12 +0700

    feat: create scroll top button feature for all pages

--- components/LayoutApp.jsx
+++ components/LayoutApp.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import cn from 'classnames';
+import throttle from '../scripts/libs/throttle';
+import ScrollTopButton from './ScrollTopButton';
 
 export default function LayoutApp({ children }) {
+  const [showScrollTop, setShowScrollTop] = useState(false);
+  useEffect(() => {
+    const scrollHandler = throttle(() => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > 479) setShowScrollTop(true);
+      else setShowScrollTop(false);
+    }, 300);
+
+    window.addEventListener('scroll', scrollHandler);
+
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
   return (
     <div className='LayoutApp'>
       <Head>
@@ -14,6 +33,16 @@ export default function LayoutApp({ children }) {
       </Head>
 
       <div className='PageApp'>{children}</div>
+
+      <div
+        className={cn(
+          'fixed bottom-4 right-4 transition duration-300',
+          { 'translate-x-24': !showScrollTop },
+          { 'translate-x-0': showScrollTop }
+        )}
+      >
+        <ScrollTopButton />
+      </div>
     </div>
   );
 }
