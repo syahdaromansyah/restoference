d83121898c74654124eb5dfc169fcb9354257deb
Syahda Romansyah <syahdarom@gmail.com>
2022-05-02 16:24:30 +0700

    feat: create custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import PrimaryLink from '../components/PrimaryLink';
+
+export default function NotFound() {
+  const [pagePath, setPagePath] = useState('');
+  const nextRouter = useRouter();
+
+  useEffect(() => {
+    setPagePath(nextRouter.asPath);
+  }, [nextRouter.asPath]);
+
+  return (
+    <div className='NotFoundPage'>
+      <div className='container mx-auto px-8 h-screen w-full overflow-hidden'>
+        <div className='flex justify-center items-center h-full'>
+          <div className='break-words text-center w-full'>
+            <h1 className='font-raleway font-bold text-4xl 2xs:text-5xl mb-2 md:mb-4'>
+              Oops! Page Not Found.
+            </h1>
+
+            <p className='2xs:text-xl mb-6'>
+              Sorry, <span className='font-bold'>{pagePath}</span> is not found.
+            </p>
+
+            <Link href='/' passHref>
+              <PrimaryLink>Back to homepage</PrimaryLink>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
